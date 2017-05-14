e0ab737b8eca95903b0c092895dfb4c92be1cd5b
mochan-tk <takashi1.kawamoto@gmail.com>
2017-05-14 20:20:17 +0900

    https://goo.gl/9GPvdt ASP.NET MVC 掲示板作成 ハンズオン 土日祝日Ver

--- WebApplication1/App_Start/Startup.Auth.cs
+++ WebApplication1/App_Start/Startup.Auth.cs
@@ -6,6 +6,8 @@ using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.Google;
 using Owin;
 using WebApplication1.Models;
+using Microsoft.Owin.Security.Twitter;
+using Microsoft.Owin.Security;
 
 namespace WebApplication1
 {
@@ -50,9 +52,22 @@ namespace WebApplication1
             //    clientId: "",
             //    clientSecret: "");
 
-            //app.UseTwitterAuthentication(
-            //   consumerKey: "",
-            //   consumerSecret: "");
+            app.UseTwitterAuthentication(new TwitterAuthenticationOptions
+            {
+                ConsumerKey = "DJwoClEnUc9JW7iwcsBc7PyWU",
+                ConsumerSecret = "MV7xyyLA50ufbgXTb9mZ7d94c81D8LIv60iRCZuxWanUA0rtfV",
+                BackchannelCertificateValidator = new CertificateSubjectKeyIdentifierValidator(new[]
+                {
+        "A5EF0B11CEC04103A34A659048B21CE0572D7D47", // VeriSign Class 3 Secure Server CA - G2
+        "0D445C165344C1827E1D20AB25F40163D8BE79A5", // VeriSign Class 3 Secure Server CA - G3
+        "7FD365A7C2DDECBBF03009F34339FA02AF333133", // VeriSign Class 3 Public Primary Certification Authority - G5
+        "39A55D933676616E73A761DFA16A7E59CDE66FAD", // Symantec Class 3 Secure Server CA - G4
+        "add53f6680fe66e383cbac3e60922e3b4c412bed", // Symantec Class 3 EV SSL CA - G3
+        "4eb6d578499b1ccf5f581ead56be3d9b6744a5e5", // VeriSign Class 3 Primary CA - G5
+        "5168FF90AF0207753CCCD9656462A212B859723B", // DigiCert SHA2 High Assurance Server CA
+        "B13EC36903F8BF4701D498261A0802EF63642BC3" // DigiCert High Assurance EV Root CA
+    })
+            });
 
             //app.UseFacebookAuthentication(
             //   appId: "",
---
+++ WebApplication1/Models/Comment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }           // プライマリキー
+        public string UserName { get; set; }  // 投稿ユーザ名
+        public string Body { get; set; }      // 本文
+        public DateTime Created { get; set; } // 投稿日時
+    }
+}
